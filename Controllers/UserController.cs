52f3392edbb98bee20b398824d0c56a7898b73ae
github-cloudlabsuser-1093 <github_cloudlabsuser_1093@cloudlabsaiuser.com>
2024-05-08 16:33:38 +0000

    chore: Update project references and fix UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,21 +8,29 @@ namespace CRUD_application_2.Controllers
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
         // GET: User
+
+
         public ActionResult Index()
         {
             // Implement the Index method here
+
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+
+            return View();
         }
  
         // POST: User/Create
@@ -30,6 +38,14 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
  
         // GET: User/Edit/5
@@ -37,6 +53,8 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
         // POST: User/Edit/5
@@ -48,19 +66,31 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            // Your code here...
+
+            return View(user);
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
- 
+
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+
+            var userToDelete = userlist.FirstOrDefault(x => x.Id == id);
+            
+            // Perform deletion logic here
+            
+            return RedirectToAction("Index");
         }
     }
 }
